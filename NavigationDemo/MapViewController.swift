1b6e782d16655233a0875f34e7af5d39449c54c3
qing <>
2023-11-15 18:09:37 +0800

    feat: CLLocationManagerDelegate 实现

--- NavigationDemo/MapViewController.swift
+++ NavigationDemo/MapViewController.swift
@@ -22,31 +22,45 @@ class MapViewController: UIViewController {
     var isNavigating = false
     var tripStartTimer: Date?
     
+    var preciseLocationZoomLevel: Float = 15.0
+    var approximateLocationZoomLevel: Float = 10.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // setup locationManager
+        setupLocationManager()
+        setupMapView()
+    }
+    
+    func setupLocationManager() {
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
 //        locationManager.distanceFilter = 50
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
-        // setup mapView
+    }
+    
+    func setupMapView() {
         let position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
         let options = GMSMapViewOptions()
-        options.camera = GMSCameraPosition.camera(withLatitude: position.latitude, longitude: position.longitude, zoom: 6.0)
+        let zoomLevel = locationManager.accuracyAuthorization == .fullAccuracy ? preciseLocationZoomLevel : approximateLocationZoomLevel
+        options.camera = GMSCameraPosition.camera(withLatitude: position.latitude, longitude: position.longitude, zoom: zoomLevel)
         mapView = GMSMapView(options: options)
         mapView.delegate = self
+        mapView.settings.myLocationButton = true
+        mapView.isMyLocationEnabled = true
         view.insertSubview(mapView, at: 0)
         mapView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+        mapView.isHidden = true
+        
         // setup marker
-        let marker = GMSMarker()
-        marker.position = position
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+//        let marker = GMSMarker()
+//        marker.position = position
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = mapView
     }
 
     // 设置在地图上的目的地
@@ -84,8 +98,65 @@ extension MapViewController: GMSMapViewDelegate {
     
 }
 
-// MARK: -
+// MARK: - CLLocationManagerDelegate
 
 extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last!
+        print("Location: \(location)")
+        
+        let zoomLevel = locationManager.accuracyAuthorization == .fullAccuracy ? preciseLocationZoomLevel : approximateLocationZoomLevel
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoomLevel)
+        
+        if mapView.isHidden {
+            mapView.isHidden = false
+            mapView.camera = camera
+        } else {
+            mapView.animate(to: camera)
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        // Check accuracy authorization
+        let accuracy = manager.accuracyAuthorization
+        switch accuracy {
+        case .fullAccuracy:
+            print("Location accuracy is precise.")
+        case .reducedAccuracy:
+            print("Location accuracy is not precise.")
+        @unknown default:
+            fatalError()
+        }
+        
+        // Handle authorization status
+        switch manager.authorizationStatus {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+            // Display the map using the default location
+            mapView.isHidden = false
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    // deprecated iOS14
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+//
+//    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Error: \(error)")
+    }
     
 }
